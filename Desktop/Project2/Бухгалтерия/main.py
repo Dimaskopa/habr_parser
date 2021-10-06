7eedcf6e34e77724e04799a11750b51c07c882a5
Dmitriy Medvedev <medvedev.keb@gmail.com>
2021-10-06 09:32:21 +0300

    Бухгалтерия добавлена

---
+++ Desktop/Project2/Бухгалтерия/main.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from application.salary import calculate_salary
+from application.db.people import get_employees
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    print(datetime.today().strftime('%d/%m/%Y'))
