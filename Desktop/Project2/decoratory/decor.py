0175527a7c795de26e5251dfb541974fd59a794b
Dmitriy Medvedev <medvedev.keb@gmail.com>
2021-10-26 18:51:15 +0300

    decor was changed

--- Desktop/Project2/decoratory/decor.py
+++ Desktop/Project2/decoratory/decor.py
@@ -3,21 +3,19 @@ import os
 
 def decor_func(sample):
 
-    def new_func(*args):
+    def new_func(path, *args):
         # можно попросить пользователя указать путь к файлу: path = input('укажите путь к папке с логом: ')
-        path = os.getcwd()
         with open(f'{path}/log.txt', 'a') as log:
          log.write(f'{datetime.datetime.now().strftime("%d %b %Y, %H : %M : %S")} | ')
          log.write(f'{sample.__name__} | ')
          log.write(f'{args} | ')
-         result = sample(*args)
+         result = sample(path, *args)
          return log.write(f'{str(result)}\n')
     return new_func
 
-
 @decor_func
-def summer(a,b,c):
-    result = a+b+c
-    return result
+def perimetr(file_path,a,b):
+    return 2*(a+b)
 
-summer(1,2,3)
+if __name__ == '__main__':
+    perimetr(os.getcwd(),2,3)
--- Desktop/Project2/парсинг/parser_habr.py
+++ Desktop/Project2/парсинг/parser_habr.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 KEYWORDS = ['IPv6', 'API', 'web', 'python']
 
