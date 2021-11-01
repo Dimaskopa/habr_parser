1526c58d2f43fd38b2fc47bd6a6df03e61265697
Dmitriy Medvedev <medvedev.keb@gmail.com>
2021-11-01 09:32:53 +0300

    decor was changed and fixed

--- Desktop/Project2/decoratory/decor.py
+++ Desktop/Project2/decoratory/decor.py
@@ -1,21 +1,25 @@
 import datetime
 import os
 
-def decor_func(sample):
+def paramtrized_decor(path):
 
-    def new_func(path, *args):
-        # можно попросить пользователя указать путь к файлу: path = input('укажите путь к папке с логом: ')
-        with open(f'{path}/log.txt', 'a') as log:
-         log.write(f'{datetime.datetime.now().strftime("%d %b %Y, %H : %M : %S")} | ')
-         log.write(f'{sample.__name__} | ')
-         log.write(f'{args} | ')
-         result = sample(path, *args)
-         return log.write(f'{str(result)}\n')
-    return new_func
+    def decor_func(sample):
 
-@decor_func
-def perimetr(file_path,a,b):
+        def new_func(*args):
+            with open(f'{path}/log.txt', 'a') as log:
+             log.write(f'{datetime.datetime.now().strftime("%d %b %Y, %H : %M : %S")} | ')
+             log.write(f'{sample.__name__} | ')
+             log.write(f'{args} | ')
+             result = sample(*args)
+             log.write(f'{str(result)}\n')
+             return result
+        return new_func
+    return decor_func
+
+@paramtrized_decor(path=os.getcwd())
+def perimetr(a,b):
     return 2*(a+b)
 
+
 if __name__ == '__main__':
-    perimetr(os.getcwd(),2,3)
+    print(perimetr(2,3))
